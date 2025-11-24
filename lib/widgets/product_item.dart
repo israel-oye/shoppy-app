c599e637cc7b967bba25f9d3e31466f9adba34ff
israel-oye <ioyeboade@gmail.com>
2025-11-24 16:38:42 +0100

    Add item detail page

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -4,67 +4,34 @@ import 'package:fetch_be/providers/cart_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ProductItem extends StatefulWidget {
+class ProductItem extends StatelessWidget {
   final Product product;
 
   const ProductItem({super.key, required this.product});
 
   @override
-  State<ProductItem> createState() => _ProductItemState();
-}
-
-class _ProductItemState extends State<ProductItem> {
-  
-  @override
   Widget build(BuildContext context) {
     final provider = context.watch<CartProvider>();
     return ListTile(
-      leading: CircleAvatar(backgroundColor: widget.product.color,),
-      title: Text(widget.product.name),
-      subtitle: Text('₦ ${widget.product.price.toStringAsFixed(2)}'),
+      onLongPress: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (_) => ItemPage(item: product)),
+        );
+      },
+      leading: CircleAvatar(backgroundColor: product.color,),
+      title: Text(product.name),
+      subtitle: Text('₦ ${product.price.toStringAsFixed(2)}'),
       trailing: Checkbox(
-        value: provider.items.contains(widget.product),
+        value: provider.items.contains(product),
         onChanged: (val){
           if (val == true) {
-            provider.addToCart(widget.product);
+            provider.addToCart(product);
           } else {
-            provider.removeItem(widget.product);
+            provider.removeItem(product);
           }
         }
       ),
-      // trailing: Row(
-      //   mainAxisSize: MainAxisSize.min,
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: [
-      //     IconButton(
-      //       onPressed: (){}, 
-      //       icon: Icon(Icons.add),
-      //     ),
-      //     SizedBox(width: 6,),
-      //     Container(
-      //       padding: const EdgeInsets.all(6),
-      //       decoration: BoxDecoration(
-      //         shape: BoxShape.circle,
-      //         border: Border.all(
-      //           color: Theme.of(context).colorScheme.primaryContainer,
-      //           width: 1.5,
-      //         ),
-      //       ),
-      //       child: IconButton(
-      //         icon: const Icon(
-      //           Icons.arrow_forward_ios,
-      //           size: 16,
-      //         ),
-      //         onPressed: (){
-      //           Navigator.push(
-      //             context, 
-      //             MaterialPageRoute(builder: (_)=>ItemPage(item: product))
-      //           );
-      //         },
-      //       ),
-      //     ),
-      //   ],
-      // ),
     );
   }
 }
